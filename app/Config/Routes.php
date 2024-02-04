ae6b0ea89d48a36eb01581c46216735fd59829e2
amitkasabe <amit.kasabe25@gmail.com>
2024-02-04 16:41:25 +0530

    rotes and controllers updated and crud is done

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->post('/insert', 'UserController::InsertUser');
+$routes->get('/users' , 'UserController::selectUsers');
+$routes->post('/users/delete' , 'UserController::delete');
+$routes->post('/users/update' , 'UserController::update');
